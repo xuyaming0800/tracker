ddc75678e11e3c08e1e7ed87c676f7fb8d0f8d8e
Xu Yaming <xuyaming0800@126.com>
2016-05-26 10:03:06 +0800

    打分支

--- src/main/java/cn/com/leador/mapapi/tracker/util/Test.java
+++ src/main/java/cn/com/leador/mapapi/tracker/util/Test.java
@@ -1,15 +1,12 @@
 package cn.com.leador.mapapi.tracker.util;
 
-import java.util.Date;
-
-import cn.com.leador.mapapi.tracker.component.CoordUtilComponent;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CoordUtilComponent cc=new CoordUtilComponent();
-		System.out.println(cc.getDirection(1, 1, 1, 1));
+//		CoordUtilComponent cc=new CoordUtilComponent();
+//		System.out.println(cc.getDirection(1, 1, 1, 1));
 
 	}
 
